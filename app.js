f5fb5fecb51d089343dafbcb6a27ab2b26cf85cf
Paul Fitzgerald <paulfitz99@gmail.com>
2018-09-21 14:45:18 +0100

    get details of all teams

--- app.js
+++ app.js
@@ -3,20 +3,6 @@ const readline = require('readline')
 const { google } = require('googleapis')
 const MongoClient = require('mongodb').MongoClient
 const url = "mongodb://paulfitz:one2345@ds111063.mlab.com:11063/premier-league-18-19"
-// Connect to the db
-MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-  if (err) throw err;
-  console.log('Connected')
-  const db = client.db('premier-league-18-19')
-  // delete the database
-  db.dropDatabase(function(err, result){
-    console.log(`Error : ${err}`)
-    if (err) throw err
-    console.log(`Operation Success ${result}`)
-    // after all the operations with db, close it.
-    client.close()
-  })
-})
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 const TOKEN_PATH = 'token.json'
@@ -67,69 +53,116 @@ const getAccessToken = (oAuth2Client, callback) => {
 }
 
 const listEvents = auth => {
-  const calendar = google.calendar('v3');
-  calendar.calendarList.list({
-    auth,
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 100,
-    singleEvents: true,
-    orderBy: 'startTime'
-  }, (err, response) => {
-    if (err) {
-      console.log('The API returned an error: ' + err)
-      return
-    }
 
-    const events = response.data.items
+  // Connect to the db
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    if (err) throw err;
+    console.log('Connected')
+    const db = client.db('premier-league-18-19')
+    // delete the database
+    db.dropDatabase(function(err, result){
+      if (err) throw err
+      console.log(`Database Dropped Success ${result}`)
 
-    if (events.length == 0) {
-      console.log('No upcoming events found.')
-    } else {
-      for (let i = 0; i < events.length; i++) {
-        calendar.events.list({
-          auth,
-          calendarId: events[i].id,
-          timeMin: (new Date('2018/06/12')).toISOString(),
-          maxResults: 2500,
-          singleEvents: true,
-          orderBy: 'startTime'
-        }, (err, response) => {
-          if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
-          }
+      const calendar = google.calendar('v3');
+      calendar.calendarList.list({
+        auth,
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 100,
+        singleEvents: true,
+        orderBy: 'startTime'
+      }, (err, response) => {
+        if (err) {
+          console.log(`The API returned an error: ${err}`)
+          return
+        }
+    
+        const events = response.data.items
+    
+        if (events.length == 0) {
+          console.log('No upcoming events found.')
+        } else {
+          for (let i = 0; i < events.length; i++) {
+            calendar.events.list({
+              auth,
+              calendarId: events[i].id,
+              timeMin: (new Date('2018/06/12')).toISOString(),
+              maxResults: 2500,
+              singleEvents: true,
+              orderBy: 'startTime'
+            }, (err, response) => {
+              if (err) {
+                console.log('The API returned an error: ' + err)
+                return
+              }
+    
+              const events = response.data.items
+              const teamName = events[i].organizer.displayName
+    
+              if (teamName !== undefined && teamName !== 'Contacts') {  
+                const obj = {
+                  teamName
+                }
 
-          const events = response.data.items
-          const teamName = events[i].organizer.displayName
-
-          if(teamName !== undefined && teamName !== 'Contacts') {
-            console.log(teamName)
-            const team = {
-              teamName,
-              teamEvents: []
-            }
-
-            MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-  
-              let obj = {
-                name: teamName
-              };
-  
-              if (err) throw err;
-              const db = client.db('premier-league-18-19');
-      
-         
-              db.collection('teams').insertOne(obj, function(err, res) {
                 if (err) throw err;
-                console.log("1 document inserted");
-                client.close();
-              });
+                db.collection('teams').insertOne(obj, function(err, res) {
+                  if (err) throw err;
+                  console.log(`${teamName} inserted`);
+                })
+              }
+    
+              if (events.length == 0) {
+                console.log('No upcoming events found.');
+              } else if(events[i].organizer.displayName && events[i].organizer.displayName.length > 0){ 
+      
+                for (let i = 0; i < events.length; i++) {
+                  if(events[i].organizer.displayName !== 'Contacts') {
+                    let event = events[i]
+                    let game = event.summary
+                    if(game.includes('[')){
+                      continue;
+                    }
+      
+                    let start = event.start.dateTime || event.start.date
+                    let opponent = event.summary.split('-').filter(team => !team.includes(teamName))[0].split('(')[0].trim()
+                    let home_or_away = event.summary.split('-')[0].includes(teamName) ? 'home' : 'away'
+                    let score = game.includes('(') ? game.split(' ').slice(-1)[0].replace(/\(|\)/g,'') : ''
+                    let winLossDraw
+                    if(score.split('-').length > 0) {
+                      winLossDraw = home_or_away === 'home' && parseInt(score.split('-')[0]) > parseInt(score.split('-')[1]) ? 'won' :
+                        home_or_away === 'away' && parseInt(score.split('-')[1]) > parseInt(score.split('-')[0]) ? 'won' :
+                        parseInt(score.split('-')[1]) == parseInt(score.split('-')[0]) ? 'draw' :
+                        score.split('-').length == 1 ? '' : 'lost'
+                    }
+    
+                    const teamDataObj = {
+                      game,
+                      start,
+                      opponent,
+                      home_or_away,
+                      score,
+                      winLossDraw
+                    }
               
-            });
+                    if (err) throw err;
+                    const db = client.db('premier-league-18-19')
+                    db.collection(teamName).insertOne(teamDataObj, function(err, res) {
+                      if (err) throw err
+                      console.log(`${JSON.stringify(teamDataObj)} inserted`)
+                    })                   
+                  }
+                }
+              }
+            })
           }
-        })
-      }
-    }
+        }
+      })
+    })
+
+    // close client when close program
+    process.on('SIGINT', function() {
+      client.close()
+    })
   })
 }
