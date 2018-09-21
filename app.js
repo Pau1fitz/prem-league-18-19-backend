1a4222bef5d1b57b9d9fa63d31b2d5b30b838413
Paul Fitzgerald <paulfitz99@gmail.com>
2018-09-21 15:02:22 +0100

    add league table scraper

--- app.js
+++ app.js
@@ -64,6 +64,80 @@ const listEvents = auth => {
       if (err) throw err
       console.log(`Database Dropped Success ${result}`)
 
+      let leagueTableUrl = 'http://www.espn.co.uk/football/table/_/league/eng.1'
+      
+      request(leagueTableUrl, function (error, response, body) {
+        if(!error){
+          var $ = cheerio.load(body);
+          let table = [];
+          var data = $('.standings');
+
+          $('.team-names').each(function(i, elm) {
+            var obj = {};
+            obj.team = $(this).text();
+            table[i] = obj;
+          });
+
+          $('.standings-row abbr').each(function(i, elm) {
+			      table[i].abbr = $(this).text();
+		      });
+
+          $('.standings-row > td:nth-child(2)').each(function(i, elm) {
+            table[i].gamesPlayed = $(this).text();
+          });
+
+          $('.standings-row > td:nth-child(3)').each(function(i, elm) {
+            table[i].won = $(this).text();
+          });
+
+          $('.standings-row > td:nth-child(4)').each(function(i, elm) {
+              table[i].draw = $(this).text();
+          });
+
+          $('.standings-row > td:nth-child(5)').each(function(i, elm) {
+              table[i].lost = $(this).text();
+          });
+
+          $('.standings-row > td:nth-last-child(2)').each(function(i, elm) {
+              table[i].goalDiff = $(this).text();
+          });
+
+          $('.standings-row > td:last-child').each(function(i, elm) {
+              table[i].points = $(this).text();
+          });
+
+          for(var i = 0; i < table.length; i++) {
+            let name = table[i].team;
+            let abbr = table[i].abbr;
+            let gamesPlayed = parseInt(table[i].gamesPlayed);
+            let won = parseInt(table[i].won);
+            let draw = parseInt(table[i].draw);
+            let lost = parseInt(table[i].lost);
+            let goalDiff = parseInt(table[i].goalDiff);
+            let points = parseInt(table[i].points);
+
+            // INSERT FIXTURES INTO THE DATABASE
+
+            if (err) throw err;
+            var myobj = {
+              name,
+              abbr,
+              gamesPlayed,
+              won,
+              draw,
+              lost,
+              goalDiff,
+              points
+            };
+
+            db.collection('table').insertOne( myobj, function(err, res) {
+              if (err) throw err;
+              console.log(`${myobj} inserted`)
+            })
+          }
+        }
+      });
+     
       const calendar = google.calendar('v3');
       calendar.calendarList.list({
         auth,
@@ -159,9 +233,9 @@ const listEvents = auth => {
         }
       })
     })
-
     // close client when close program
     process.on('SIGINT', function() {
+      console.log('Connection closed')
       client.close()
     })
   })
